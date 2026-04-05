33469f48b984e56c1dcfd40c7b00fc9dc3b1859b
Luis Orozco <luiscooz98@gmail.com>
2026-04-04 20:49:03 -0600

    feat: Python fundamentals - variables and data types

---
+++ fundamentos.py
@@ -0,0 +1,18 @@
+# Variables y tipos de datos
+
+nombre = "Luis"
+edad = 30
+altura = 1.75
+es_backend_engineer = False
+
+print(type(nombre))
+print(type(edad))
+print(type(altura))
+print(type(es_backend_engineer))
+
+# Listas
+habilidades = ["Python", "Git", "GitHub", "Terminal"]
+print(habilidades)
+print(habilidades[0])
+print(habilidades[3])
+print(len(habilidades))
