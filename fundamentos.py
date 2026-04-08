6b9382eb9fdcd9ca01a5706dc56db195f5c42192
Luis Orozco <luiscooz98@gmail.com>
2026-04-07 20:23:05 -0600

    feat: data structures - lists, dicts, tuples, sets

--- fundamentos.py
+++ fundamentos.py
@@ -16,3 +16,38 @@ print(habilidades)
 print(habilidades[0])
 print(habilidades[3])
 print(len(habilidades))
+
+# Diccionarios
+habilidades.append("FastAPI")
+print(habilidades)
+
+habilidades.remove("Terminal")
+print(habilidades)
+
+print(len(habilidades))
+
+ingeniero = {
+    "nombre": "Luis",
+    "edad": 30,
+    "lenguaje": "Python",
+    "meta": "Ser un profesional en Backend Engineering"
+}
+
+ingeniero["lenguaje"] = "Python & FastAPI"
+ingeniero["ciudad"] = "Mexico City"
+
+print(ingeniero)
+print(ingeniero["lenguaje"])
+print(ingeniero["meta"])
+print(ingeniero)
+
+# Tuplas
+coordenadas = (19.4326, -99.1332)
+print(coordenadas)
+print(coordenadas[0])
+print(type(coordenadas))
+
+# Sets
+tecnologias = {"Python", "FastAPI", "Python", "Docker", "FastAPI"}
+print(tecnologias)
+print(type(tecnologias))
