68472bb022a930b9d234e60962e9cdb29c17e915
Luis Orozco <luiscooz98@gmail.com>
2026-04-20 11:23:51 -0600

    feat: add POST /ingeniero endpoint with Ingeniero schema

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-app = FastAPI
+app = FastAPI()
 
 @app.get("/")
 def root():
@@ -9,3 +10,15 @@ def root():
 @app.get("/status")
 def status():
     return {"status": "ok", "engineer": "Luis Orozco"}
+
+class Ingeniniero(BaseModel):
+    name: str
+    age: int
+    specialty: str
+
+@app.post("/engineer")
+def create_engineer(engineer: Ingeniniero):
+    return {
+        "message": f"Bienvendio {engineer.name}",
+        "data": engineer
+    }
